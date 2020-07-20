e2e3b38c278fb45c4bcf9a99d7d2498b5b1a3c1d
Konstantine Rybnikov <k-bx@k-bx.com>
2020-07-20 12:27:49 +0300

    Print path info nicely

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,16 +6,22 @@ import qualified Control.Concurrent
 import Control.Monad.IO.Class
 import Data.Monoid (mconcat)
 import Web.Scotty
+import qualified Data.String.Class as S
+import qualified Network.Wai
 
 webapp :: IO ()
-webapp = scotty 5001 $ do
-  get "/" $ do
-    hs <- headers
-    liftIO $ putStrLn $ "> GET / headers: " ++ show hs
-    liftIO $ putStrLn "> GET / waiting for 2 secs"
-    liftIO $ Control.Concurrent.threadDelay (2 * 1000000)
-    liftIO $ putStrLn "> GET / done waiting"
-    html $ mconcat ["hey there"]
-  get "/:word" $ do
-    beam <- param "word"
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+webapp = do
+  liftIO $ putStrLn "> launching..."
+  scotty 5001 $ do
+    get "/" $ do
+      req <- request
+      hs <- headers
+      let pathInfo = S.toString (Network.Wai.rawPathInfo req) ++ S.toString (Network.Wai.rawQueryString req) 
+      liftIO $ putStrLn $ "> GET " ++ pathInfo ++ " headers: " ++ show hs
+      liftIO $ putStrLn $ "> GET " ++ pathInfo ++ " waiting for 2 secs"
+      liftIO $ Control.Concurrent.threadDelay (2 * 1000000)
+      liftIO $ putStrLn $ "> GET " ++ pathInfo ++ " done waiting"
+      html $ mconcat ["hey there"]
+    get "/:word" $ do
+      beam <- param "word"
+      html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
